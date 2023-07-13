c0d0b87da06e7f91c1ca1205d1fc7b57a1a4a6ab
ShuangbiaoDai <dai123@hust.edu.cn>
2023-07-13 00:38:53 -0700

    Add files via upload
    ubuntu 20.04 base env conf.

---
+++ ubuntu20.04.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# env ubuntu20.04 base environment
+sudo apt update
+sudo apt install build-essential
+sudo apt install gcc g++
+sudo apt install python3-pip
+sudo apt install git
+sudo apt install cmake
+#...
